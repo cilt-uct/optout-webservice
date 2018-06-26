d1f421e5169b5dcee1107b4666a4f79b4151694e
slampunk <duncan83@gmail.com>
2018-06-26 13:59:38 +0200

    OPENCAST-2128: added LDAP service for authentication and user info retrieval

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,21 +3,96 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\LDAPService;
 
 class ApiController extends Controller
 {
-  /**
-   * @Route("/api")
-   */
-  public function number()
-  {
-    $number = random_int(0, 100);
 
-    return new Response(
-      "<html><body>This is a random number generated just for you: $number</body></html>"
-    );
+  /**
+   * @Route("/search/{searchStr}")
+   */
+  public function search($searchStr) {
+    $ldap = new LDAPService();
+    try {
+      $result = $ldap->match($searchStr);
+      return new Response(
+        json_encode($result),
+        200,
+        [
+          'Content-Type' => 'application/json'
+        ]
+      );
+    } catch (\Exception $e) {
+      $response = [
+        "text" => "Server error",
+        "statusCode" => 500,
+        "contentType" => [
+          'Content-Type' => 'text/plain'
+        ]
+      ];
+      switch($e->getMessage()) {
+        case "no such user":
+          $response['text'] = 'User not found';
+          $response['statusCode'] = 404;
+      }
+
+      return new Response($response['text'], $response['statusCode'], $response['contentType']);
+    }
   }
-    
+
+  /**
+   * @Route("/authenticate")
+   */
+  public function auth(Request $request) {
+    switch ($request->getMethod()) {
+      case 'POST':
+        return $this->handleAuthentication($request);
+        break;
+
+      default:
+        return $this->displayAuthenticationForm($request);
+    }
+  }
+
+  private function handleAuthentication($request) {
+    $ldap = new LDAPService();
+    $user = $request->request->get('username');
+    $password = $request->request->get('password');
+
+    try {
+      if ($ldap->authenticate($user, $password)) {
+        return new Response("OK", 200);
+      }
+
+      return new Response("Invalid username/password combination", 401);
+
+    } catch (\Exception $e) {
+      $response = [
+        "text" => "Server error",
+        "statusCode" => 500,
+        "contentType" => [
+          'Content-Type' => 'text/plain'
+        ]
+      ];
+      switch ($e->getMessage()) {
+        case 'no such user':
+          $response['text'] = 'No such user';
+          $response['statusCode'] = 404;
+          break;
+
+        case 'invalid id':
+          $response['text'] = 'Please use your official UCT staff number';
+          $response['statusCode'] = 400;
+          break;
+      }
+      return new Response($response['text'], $response['statusCode'], $response['contentType']);
+    }
+  }
+
+  private function displayAuthenticationForm(Request $request) {
+    return new Response("<form method=post><p><input name=username /></p><p><input name=password type=password /></p><button>Login</button></form>");
+  }
 }
---
+++ src/Service/LDAPService.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Service;
+
+class LDAPService
+{
+
+    //TODO: add ldap variables to .env, e.g. o=uct as base dn
+    private $ldapConn;
+    private $returnFields = [
+       'alternateemail'
+      ,'preferredname'
+      ,'mobile'
+      ,'cn'
+      ,'sn'
+      ,'dn'
+    ];
+
+    public function __construct($host = '', $user = '', $pass = '') {
+        if (empty($host)) {
+            $this->ldapConn = \ldap_connect('ldaps://ldap.uct.ac.za', 636);
+        }
+        else {
+            $this->ldapConn = \ldap_connect($host, 636);
+        }
+    }
+
+    public function search($filter) {
+        $ldapSearch = \ldap_search($this->ldapConn, 'o=uct', $filter);
+        if ($ldapSearch != false) {
+            $ldapSearchArray = ldap_get_entries($this->ldapConn, $ldapSearch);
+            if (!$ldapSearchArray['count']) {
+                throw new \Exception("no such user");
+            }
+            $searchResults = [];
+            foreach ($ldapSearchArray as $key => $val) {
+              if (is_numeric($key)) {
+                $result = [];
+                foreach ($this->returnFields as $idx => $field) {
+                  $result[$field] = is_array($ldapSearchArray[$key][$field]) ? $ldapSearchArray[$key][$field]['0'] : $ldapSearchArray[$key][$field];
+                }
+                $searchResults[] = $result;
+              }
+            }
+            return $searchResults;
+        }
+        else {
+            throw new \Exception("cannot connect to ldap server");
+        }
+    }
+
+    public function match($search) {
+        if (filter_var($search, FILTER_VALIDATE_EMAIL)) {
+            $uctEmailSuffixPosition = strpos($search, '@uct.ac.za');
+            if ($uctEmailSuffixPosition > -1) {
+                $nameArr = explode('.', substr($search, 0, $uctEmailSuffixPosition));
+                $firstname = $nameArr[0];
+                $surname = implode(' ', array_splice($nameArr, 1));
+                $filter = "(&(preferredName=$firstname)(sn=$surname))";
+            }
+            else {
+                $filter = "(alternateEmail=$search)";
+            }
+        }
+        else {
+            $filter = "(cn=$search)";
+        }
+
+        return $this->search($filter);
+    }
+
+    public function authenticate($username, $password) {
+        if (!is_numeric($username)) {
+          throw new \Exception('invalid id');
+        }
+        try {
+            $searchUser = ($this->match($username))[0];
+            //Match Exact?! shouldn't infer from $username. Needs to be staff number
+            if (@\ldap_bind($this->ldapConn, $searchUser['dn'], $password)) {
+                return true;
+            }
+            return false;
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+            switch ($e->getMessage()) {
+                case 'no such user':
+                    throw new \Exception($e->getMessage());
+            }
+        }
+    }
+}
