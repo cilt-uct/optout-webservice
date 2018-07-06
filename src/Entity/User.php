1c38d81dca333bae13a49f5b2434acd2e4721b22
slampunk <duncan83@gmail.com>
2018-07-06 14:27:47 +0200

    OPENCAST-1454: added full search functionality using Vula DB

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,9 +11,17 @@ class User
     private $email;
 
     public function __construct($eid, $ip = null, $email = null) {
-        $this->eid = $eid;
-        $this->ip = $ip;
-        $this->email = !!$email ? $email : (new Utilities())->getUserEmail($eid);
+        if (!is_null($eid)) {
+            $this->eid = $eid;
+            $this->ip = $ip;
+            $this->email = !!$email ? $email : (new Utilities())->getUserEmail($eid);
+        }
+        else if (!is_null($email)) {
+            $searchResult = (new Utilities())->getCompleteUser($email);
+            $this->eid = $searchResult['EID'];
+            $this->email = $searchResult['EMAIL'];
+
+        }
     }
 
     public function getDetails() {
